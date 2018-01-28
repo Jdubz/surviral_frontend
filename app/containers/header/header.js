18df5716bf9963317d290437aeed7f48e73392f6
Thomas Johnston <trjohnst211@gmail.com>
2018-01-27 19:05:56 -0800

    Adding material ui and restyling nav

--- app/containers/header/header.js
+++ app/containers/header/header.js
@@ -3,23 +3,40 @@ import {
   observer
 } from 'globalImports';
 
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
 import navStore from '../../stores/navStore';
 
 @observer
 class MyMenu extends React.Component {
   render() {
     return (<div className="menu-container" >
-      <h2>SurViral</h2>
-      <button
-        onClick={() => {
-          navStore.changePage('home');
-        }}
-      >home</button>
-      <button
-        onClick={() => {
-          navStore.changePage('explore');
-        }}
-      >explore</button>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography type="title" color="inherit">
+            SurViral
+          </Typography>
+          <Button
+            color="inherit"
+            onClick={() => {
+              navStore.changePage('home');
+            }}
+          >
+            Home
+          </Button>
+          <Button
+            color="inherit"
+            onClick={() => {
+              navStore.changePage('explore');
+            }}
+          >
+            Explore
+          </Button>
+        </Toolbar>
+
+      </AppBar>
     </div>);
   }
 }
